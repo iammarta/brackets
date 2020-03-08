fbe1d3dfd988c8bc00033359eaa4c5a9695fa411
Marta Kozak <51427243+iammarta@users.noreply.github.com>
2020-03-08 19:51:00 +0200

    init: upload the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let obj = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "|": "|",
+    "1": "2",
+    "3": "4",
+    "5": "6",
+    "7": "7",
+    "8": "8"
+  };
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+      if (arr[arr.length - 1] !== str[i]){
+          arr.push(obj[str[i]]);
+      }else if (arr[arr.length - 1] === str[i]){
+          arr.pop();
+      }else{
+        return false;
+      }
+  }
+  return arr.length === 0;
+};
